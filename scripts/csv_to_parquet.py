fd84385c5a7de2010e61df206d65d9ad6e68bdc0
MateusVasc <mvaraujo2004@gmail.com>
2025-01-27 19:29:29 -0300

    feat: Adds parquet parser script

---
+++ scripts/csv_to_parquet.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+def csv_to_parquet(path_csv, path_final="../data"):
+    df = pd.read_csv(path_csv, sep=';', on_bad_lines='warn')
+
+    if not os.path.exists(path_final):
+        os.makedirs(path_final)
+
+    path_arquivo_parquet = os.path.join(path_final, os.path.splitext(os.path.basename(path_csv))[0] + ".parquet")
+    
+    df.to_parquet(path_arquivo_parquet, engine="pyarrow")
+    print(f'Arquivo salvo em: {path_arquivo_parquet}')
+
+
+csv_to_parquet("../data/Dados_commodities_mensal_nova.csv")
