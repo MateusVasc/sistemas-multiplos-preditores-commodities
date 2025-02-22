1030f4fcd3fdfbbf5e0d25f8404cb04789f02d86
MateusVasc <mvaraujo2004@gmail.com>
2025-02-22 14:53:33 -0300

    docs: Adds docs to conversion script

--- scripts/csv_to_parquet.py
+++ scripts/csv_to_parquet.py
@@ -2,6 +2,13 @@ import pandas as pd
 import os
 
 def csv_to_parquet(path_csv, path_final="../data"):
+    """
+    Converte um arquivo CSV para o formato Parquet e o salva no diretório especificado.
+    
+    :param path_csv: Caminho do arquivo CSV de entrada.
+    :param path_final: Diretório onde o arquivo Parquet será salvo (padrão: "../data").
+    :return: Nenhum retorno explícito, mas salva o arquivo convertido no local indicado.
+    """
     df = pd.read_csv(path_csv, sep=';', on_bad_lines='warn')
 
     if not os.path.exists(path_final):
