88f7d41f8137efd9d65ab28337b6573821b7beaa
MateusVasc <mvaraujo2004@gmail.com>
2025-02-22 14:50:39 -0300

    feat: Adds function to convert dataset data to date

---
+++ scripts/convert_ds_to_date.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def convert_ds_to_date(df):
+    """
+    Converte a coluna 'ds' para o formato de data (datetime).
+    
+    :param df: DataFrame contendo a coluna 'ds'.
+    :return: DataFrame com 'ds' convertido para datetime.
+    """
+    df = df.copy()  # Evita modificar o original
+    df['ds'] = pd.to_datetime(df['ds'], format='%d/%m/%Y', errors='coerce')
+    return df
