3aa76b16e0dea1e41bf6231e6d8302f3f7422c37
MateusVasc <mvaraujo2004@gmail.com>
2025-05-10 21:27:12 -0300

    refac: merges updated commodities data

--- scripts/merge_data.py
+++ scripts/merge_data.py
@@ -4,38 +4,31 @@ import sys
 import os
 sys.path.append(os.path.abspath("../"))
 
-from scripts.save_to_parquet import save_to_parquet
+from scripts.time_series_preprocessing import export_to_parquet
 
 def merge_parquet_dfs(dfs):
-    """
-    Junta uma lista de DataFrames do Pandas verticalmente.
-    
-    :param dfs: Lista de DataFrames a serem combinados.
-    :return: DataFrame resultante da concatenação de todos os DataFrames da lista.
-    """
     if not dfs:
         raise ValueError("A lista de DataFrames está vazia.")
     
     df_merged = pd.concat(dfs, axis=0, ignore_index=True)
     return df_merged
 
-df_acucar_brl = pd.read_parquet('../data/acucar/acucar_brl.parquet')
+df_acucar_santos_brl = pd.read_parquet('../data/acucar/acucar_santos_brl.parquet')
+df_acucar_sp_brl = pd.read_parquet('../data/acucar/acucar_sp_brl.parquet')
 df_algodao_brl = pd.read_parquet('../data/algodao/algodao_brl.parquet')
-df_cafe_brl = pd.read_parquet('../data/cafe/cafe_brl.parquet')
+df_arroz_brl = pd.read_parquet('../data/arroz/arroz_brl.parquet')
+df_cafe_arabica_brl = pd.read_parquet('../data/cafe/cafe_arabica_brl.parquet')
+df_cafe_robusta_brl = pd.read_parquet('../data/cafe/cafe_robusta_brl.parquet')
 df_milho_brl = pd.read_parquet('../data/milho/milho_brl.parquet')
-df_soja_brl = pd.read_parquet('../data/soja/soja_brl.parquet')
+df_soja_parana_brl = pd.read_parquet('../data/soja/soja_parana_brl.parquet')
+df_soja_paranagua_brl = pd.read_parquet('../data/soja/soja_paranagua_brl.parquet')
+df_trigo_parana_brl = pd.read_parquet('../data/trigo/trigo_parana_brl.parquet')
+df_trigo_rs_brl = pd.read_parquet('../data/trigo/trigo_rs_brl.parquet')
 
-df_acucar_usd = pd.read_parquet('../data/acucar/acucar_usd.parquet')
-df_algodao_usd = pd.read_parquet('../data/algodao/algodao_usd.parquet')
-df_cafe_usd = pd.read_parquet('../data/cafe/cafe_usd.parquet')
-df_milho_usd = pd.read_parquet('../data/milho/milho_usd.parquet')
-df_soja_usd = pd.read_parquet('../data/soja/soja_usd.parquet')
 
-dfs_brl = [df_acucar_brl, df_algodao_brl, df_cafe_brl, df_milho_brl, df_soja_brl]
-dfs_usd = [df_acucar_usd, df_algodao_usd, df_cafe_usd, df_milho_usd, df_soja_usd]
+dfs_brl = [df_acucar_santos_brl, df_acucar_sp_brl, df_algodao_brl, df_arroz_brl, df_cafe_arabica_brl, df_cafe_robusta_brl, df_milho_brl,
+           df_soja_parana_brl, df_soja_paranagua_brl, df_trigo_parana_brl, df_trigo_rs_brl]
 
 df_brl_final = merge_parquet_dfs(dfs_brl)
-df_usd_final = merge_parquet_dfs(dfs_usd)
 
-save_to_parquet(df_brl_final, "../data/all_comm/comm_brl.parquet")
-save_to_parquet(df_usd_final, "../data/all_comm/comm_usd.parquet")
+export_to_parquet(df_brl_final, "../data/all_comm/all_commodities_brl.parquet")
